e085c297b518a96651635f2b9a9166ca76458ee6
z-lew <lewiszachary22@yahoo.com>
2021-10-14 15:07:56 -0400

    getting started

--- Project_code/POS Ordering System/POS Ordering System/Form1.cs
+++ Project_code/POS Ordering System/POS Ordering System/Form1.cs
@@ -12,9 +12,28 @@ namespace POS_Ordering_System
 {
     public partial class Form1 : Form
     {
+        List<Panel> listPanel = new List<Panel> ();
+        int index;
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (index < listPanel.Count - 1) listPanel[index++].BringToFront();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listPanel.Add(flowLayoutPanel1);
+            listPanel.Add(flowLayoutPanel2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index > 0) listPanel[--index].BringToFront();
         }
     }
 }
+ 
