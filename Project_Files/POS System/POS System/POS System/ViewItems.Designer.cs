0a0f09affa4709797c11e633092badb04e82e0c9
jane liang <liangqian1994@gmail.com>
2021-11-10 17:41:27 -0500

    add combo box

--- Project_Files/POS System/POS System/POS System/ViewItems.Designer.cs
+++ Project_Files/POS System/POS System/POS System/ViewItems.Designer.cs
@@ -30,25 +30,39 @@ namespace POS_System
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Filter = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1144, 519);
+            this.dataGridView1.Size = new System.Drawing.Size(1144, 455);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Filter
+            // 
+            this.Filter.FormattingEnabled = true;
+            this.Filter.Location = new System.Drawing.Point(12, 491);
+            this.Filter.Name = "Filter";
+            this.Filter.Size = new System.Drawing.Size(310, 24);
+            this.Filter.TabIndex = 1;
+            this.Filter.SelectedIndexChanged += new System.EventHandler(this.Filter_SelectedIndexChanged);
+            this.Filter.SelectionChangeCommitted += new System.EventHandler(this.FilterList);
             // 
             // ViewItems
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1168, 667);
+            this.Controls.Add(this.Filter);
             this.Controls.Add(this.dataGridView1);
             this.Name = "ViewItems";
             this.Text = "View Items";
@@ -60,5 +74,6 @@ namespace POS_System
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox Filter;
     }
 }
--- Project_Files/POS System/POS System/POS System/ViewItems.cs
+++ Project_Files/POS System/POS System/POS System/ViewItems.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Core.Objects;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,11 +20,32 @@ namespace POS_System
 
             dataGridView1.DataSource = pos.TBLmenuItems.ToList();
 
+            dataGridView1.Columns["MenuType"].Visible = false;
+            dataGridView1.Columns["TbProductType"].Visible = false;
+            dataGridView1.Columns["TBLorderItems"].Visible = false;
 
+            Filter.DataSource = pos.TbProductTypes.ToList();
+            Filter.DisplayMember = "Description";
+            Filter.ValueMember = "ProductType";
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+
+        }
+
+        private void Filter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterList(object sender, EventArgs e)
+        {
+           
+                ObjectQuery<TBLmenuItem> filteredMnueItem = new ObjectQuery<TBLmenuItem>(
+                    "SELECT VALUE C FROM TBLmenuItems AS C WHERE C.ProductType = " + Filter.SelectedValue, pos);
+            
+
 
         }
     }
